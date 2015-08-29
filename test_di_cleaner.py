0fd98763a6252ba45a23f12a8dc0fcb6aa07bd27
Alejandro E. Brito Monedero <alejandro.monedero@gmail.com>
2015-08-29 13:42:58 +0200

    Add tests for split_images

--- test_di_cleaner.py
+++ test_di_cleaner.py
@@ -13,8 +13,7 @@ class TestDockerImageCleanerMethods(unittest.TestCase):
         none_images = [image_none_dummy]
         non_none_images = [image_non_none_dummy]
         non_none_images, none_images = di_cleaner.split_by_none(
-            non_none_images,
-            none_images,
+            (non_none_images, none_images),
             image_none)
         self.assertIn(
             image_none,
@@ -29,8 +28,7 @@ class TestDockerImageCleanerMethods(unittest.TestCase):
             non_none_images,
             msg='None image should not be in non_none_images list')
         non_none_images, none_images = di_cleaner.split_by_none(
-            non_none_images,
-            none_images,
+            (non_none_images, none_images),
             image_non_none)
         self.assertIn(
             image_non_none,
@@ -44,6 +42,23 @@ class TestDockerImageCleanerMethods(unittest.TestCase):
             image_non_none,
             none_images,
             msg='Non None image should not be in none_images list')
+        with self.assertRaises(TypeError):
+            di_cleaner.split_by_none(non_none_images, image_non_none)
+        with self.assertRaises(ValueError):
+            di_cleaner.split_by_none((non_none_images,), image_non_none)
+            di_cleaner.split_by_none((non_none_images, none_images, none_images),
+                                     image_non_none)
+
+
+    def test_split_images(self):
+        images = [{'id': id_, u'RepoTags': u'<none>:<none>'}
+                  for id_ in range(2)]
+        images.append({'id': 2, u'RepoTags': u'<alfa>:<omega>'})
+        non_none_images, none_images = di_cleaner.split_images(images)
+        self.assertEqual(len(non_none_images), 1,
+                         msg='non_none_images should have one element')
+        self.assertEqual(len(none_images), 2,
+                         msg='none_images should have two elements')
 
 
 if __name__ == '__main__':
